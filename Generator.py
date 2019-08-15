502549a36d3918502d602471662522e81a539cf5
suku1 <30659952+suku1@users.noreply.github.com>
2019-08-16 04:44:02 +0900

    生成枚数に上限を設定

--- Generator.py
+++ Generator.py
@@ -69,7 +69,9 @@ if __name__ == '__main__':
     num_image = 100
     l = len(sys.argv)
     if l >= 2: num_image = int(sys.argv[1])
+    num_image = min(num_image, 1000)
     #画像num_image枚分のノイズを生成
     noise = np.random.uniform(-1, 1, (num_image, 100))
+    print(noise.shape)
     #画像を生成
     gen_image(model_name, noise)
